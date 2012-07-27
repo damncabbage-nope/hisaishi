9bac02079911a671e6291947455521f240a8d00f
Geoffrey Roberts <g.roberts@blackicemedia.com>
2012-07-27 21:57:04 +1000

    Fixed reordering

--- public/scripts/sortableList.js
+++ public/scripts/sortableList.js
@@ -8,36 +8,58 @@ var SortableList = function(selector, params) {
 	}
 	
 	var $ = jQuery, 
-	sel = selector;
+	sel = $(selector);
 	
 	var settings = {
 		containment: null,
 		opacity: 0.6,
 		autoInit: false,
-		onInit: function(selObj) {
+		onInit: function(sel) {
 		},
 		onUpdate: function(event, ui) {
 		},
-		onDestroy: function(selObj) {
-		}
+		onDestroy: function(sel) {
+		},
 	};
 	$.extend(settings, params);
 	
+	var sortPrefs = {
+    axis: 'y',
+    containment: settings.containment,
+    opacity: settings.opacity,
+    update: settings.onUpdate,
+    placeholder: "ui-state-highlight"
+  };
+  
+  var currentOrder = '';
+	
 	var pub = {
 		init: function() {
-			$(sel).sortable({
-				axis: 'y',
-				containment: settings.containment,
-				opacity: settings.opacity,
-				update: settings.onUpdate,
-				placeholder: "ui-state-highlight"
-			});
-			settings.onInit($(sel));
+			pub.serialise();
+			settings.onInit(sel);
 		},
 		
 		destroy: function() {
-			$(sel).sortable('destroy');
-			settings.onDestroy($(sel));
+  		pub.serialise();
+			sel.sortable('destroy');
+			settings.onDestroy(sel);
+		},
+		
+		serialise: function() {
+		  sel.sortable(sortPrefs);
+		  // currentOrder = sel.sortable('serialize');
+		  var list = [];
+		  sel.children().map(function(){
+		    var parts = this.id.split('_');
+		    list.push(parts[0] + '[]=' + parts[1]);
+		  });
+		  
+		  currentOrder = list.join('&');
+		  return currentOrder;
+		},
+		
+		getOrder: function() {
+		  return currentOrder;
 		}
 	};
 	
